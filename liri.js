abaa171b86bde368d88d14851d006077963122b8
tylermorrisford <tylerford@gmail.com>
2019-09-10 21:44:08 -0600

    build omdb axios request/format response

--- liri.js
+++ liri.js
@@ -12,33 +12,41 @@ var input = process.argv[3];
 
 switch ( command ) {
     case "movie-this":
-        axios.get("http://www.omdbapi.com/?t=" + input + "&apikey=trilogy").then(
+        axios.get("http://www.omdbapi.com/?t=" + input + "&tomatoes=true&apikey=trilogy").then(
             function(response) {
-                // console.log("---------------Response---------------");
-                // console.log(response);
-                // console.log("---------------Response.Data---------------");
-                // console.log(response.data);
-                // console.log("---------------Data---------------");
-                console.log(response.data);
+              console.log("\r\n\r\n\r\n");
               console.log("\"" + response.data.Title 
-              + "\"\nrelease year: " + response.data.Year 
-              + "\nIMDB Rating: " + response.data.imdbRating
-              + "\nRotten Tomatoes Rating: " + response.data.tomatoRating
+              + "\"\nRelease year: " + response.data.Year 
+              + "\nIMDB Rating: " + response.data.Ratings[0].Value
+              + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value
               + "\nCountry: " + response.data.Country
               + "\nLanguage: " + response.data.Language
+              + "\nPlot: " + response.data.Plot
               + "\nActors: " + response.data.Actors
               );
+              console.log("\r\n\r\n");
             })
             .catch(function(error) {
-              if (error.response) {
-                console.log(error.response);
-              } 
-              console.log(error.config);
-            });
-
+                console.log(error.response.data);
+              })
       break;
-    case y:
+    
+    case "spotify-this-song":
       // code block
+      break;
+
+    case "concert-this":
+        axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+            function(response) {
+              console.log("The movie's rating is: " + response.data.imdbRating);
+            })
+            .catch(function(error) {
+                console.log(error.response.data);
+              })
+      break;
+      
+    case "do-what-it-says":
+      // read the random.txt file .split(",") and read as process.argv[2] and process.argv[3]
       break;
     default:
       // code block
@@ -51,5 +59,5 @@ switch ( command ) {
 // switch statement cases:
 // - concert-this   for Bands In Town api (uses moment to render concert dates)
 // - spotify-this-song
-// - movie-this      for OMDB
+// - movie-this      for OMDB   need more tests & input validation, mostly working
 // - do-what-it-says 
